bc9e016e504434ed139799ca24772a9ca5925df1
Роман <dudoitsrv@gmail.com>
2021-02-19 11:42:32 +0300

    ex-50

---
+++ local/php_interface/events.php
@@ -0,0 +1,26 @@
+<?php
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("ElementUpdate", "checkCount"));
+
+class ElementUpdate
+{
+    function checkCount(&$arFields)
+    {
+        $arFilter = array("ID" => $arFields["ID"]);
+        $arSelect =array("ID", "IBLOCK_ID", "NAME","SHOW_COUNTER");
+
+      $result = CIBlockElement::GetList(array(),$arFilter, false,false, $arSelect);
+      if($res = $result->Fetch())
+      {
+          echo '<pre>'; var_dump($res); echo '</pre>';
+
+           if ($res["SHOW_COUNTER"] >= 2 && $arFields["ACTIVE"] == "N")
+           {
+                global $APPLICATION;
+
+                $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $res["SHOW_COUNTER"] ." просмотров");
+                return false;
+            }
+       }
+    }
+}
+?>
