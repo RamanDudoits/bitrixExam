8f4e75cf59e95652433a79a16b10feaf69a18f3b
RamanDudoits <didoitsrv@gmail.com>
2021-02-23 14:05:51 +0300

    ex2-50

--- local/php_interface/events.php
+++ local/php_interface/events.php
@@ -1,23 +1,25 @@
 <?php
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("ElementUpdate", "checkCount"));
+//AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("ElementUpdate", "checkCount"));
+//
+//class ElementUpdate
+//{
+//    function checkCount(&$arFields)
+//    {
+//        $arFilter = array("ID" => $arFields["ID"]);
+//        $arSelect = array("ID", "IBLOCK_ID", "NAME", "SHOW_COUNTER");
+//
+//        $result = CIBlockElement::GetList(array(), $arFilter, false, false, $arSelect);
+//        if ($res = $result->Fetch()) {
+//            if ($res["SHOW_COUNTER"] >= 2 && $arFields["ACTIVE"] == "N") {
+//                global $APPLICATION;
+//                $APPLICATION->throwException("Товар невозможно деактивировать, у него " . $res["SHOW_COUNTER"] . " просмотров");
+//                return false;
+//            }
+//        }
+//    }
+//}
 
-class ElementUpdate
-{
-    function checkCount(&$arFields)
-    {
-        $arFilter = array("ID" => $arFields["ID"]);
-        $arSelect =array("ID", "IBLOCK_ID", "NAME","SHOW_COUNTER");
+//echo '<pre>'; var_dump($res); echo '</pre>';
+//?>
 
-      $result = CIBlockElement::GetList(array(),$arFilter, false,false, $arSelect);
-      if($res = $result->Fetch())
-      {
-           if ($res["SHOW_COUNTER"] >= 2 && $arFields["ACTIVE"] == "N")
-           {
-                global $APPLICATION;
-                $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $res["SHOW_COUNTER"] ." просмотров");
-                return false;
-            }
-       }
-    }
-}
-?>
+
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,2 +1,20 @@
-<?
-include_once $_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/events.php";
+<?php
+
+$eventManager = \Bitrix\Main\EventManager::getInstance();
+$eventManager->addEventHandler('iblock', 'OnBeforeIBlockElementUpdate', 'checkCount');
+
+   function checkCount (&$arFields) {
+        $arFilter = array("ID" => $arFields["ID"]);
+        $arSelect =array("ID", "IBLOCK_ID", "NAME","SHOW_COUNTER");
+
+        $result = CIBlockElement::GetList(array(),$arFilter, false,false, $arSelect);
+        if($res = $result->Fetch())
+        {
+            if ($res["SHOW_COUNTER"] >= 2 && $arFields["ACTIVE"] == "N")
+            {
+                global $APPLICATION;
+                $APPLICATION->throwException("Товар невозможно деактивировать, у него ". $res["SHOW_COUNTER"] ." просмотров");
+                return false;
+            }
+        }
+   }
