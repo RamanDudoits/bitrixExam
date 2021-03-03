286e658eb23d1388a114eef9dcd3b02831d94a1d
RamanDudoits <didoitsrv@gmail.com>
2021-03-03 09:09:29 +0300

    ex2-71

---
+++ local/components/ex2/simplecomponent/draft.php
@@ -0,0 +1,20 @@
+<?php
+
+//            $rsElements = CIBlockElement::getList(array(
+//                'select' => array('ID', "IBLOCK_ID", "NAME"),
+//                'filter' => array(
+//                    "IBLOCK_ID" => $this->arParams["CLASS_IBLOCK_ID"],
+//                    "CHECK_PERMISSIONS" => $this->arParams["CACHE_GROUPS"],
+//                    "ACTIVE" => "Y"),
+//            ));
+
+
+//        echo "<pre>"; print_r( $this->arResult["CLASSIF"]); echo "</pre>";
+
+//$map =  CIBlock::GetPermission($this->arParams["CLASS_IBLOCK_ID"])
+
+//
+//D для полного запрета
+//R для чтения
+//W для записи
+//X для полного доступа
